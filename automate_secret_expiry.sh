27f78ac647f9d78cd19b0893eed4d9fd37b54508
Aliyuy <64634720+Aliyuy@users.noreply.github.com>
2024-01-29 13:50:37 -0600

    Update automate_secret_expiry.sh

--- automate_secret_expiry.sh
+++ automate_secret_expiry.sh
@@ -234,6 +234,13 @@ tar: Error is not recoverable: exiting nowtar -C /usr/local -xzf /path/to/your/h
 
 
 tar -C /usr/local -xzf /path/to/your/home/directory/go1.21.5.linux-amd64.tar.gz
+wget https://golang.org/dl/go$(curl -s https://golang.org/VERSION?m=text).linux-amd64.tar.gz
+
+
+sudo tar -C /usr/local -xzf go*.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+
 
 
 
